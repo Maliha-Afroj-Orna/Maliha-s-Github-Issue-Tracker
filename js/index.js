36acd6a5cf86506c9f0c988fd05190de52a3f954
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-07 19:09:33 +0600

    login page created

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// login page starts
+const btnSignIn = document.getElementById('btn-signIn');
+const userInput = document.getElementById('user-input');
+const userInputValue = userInput.value;
+const userPassword = document.getElementById('user-password');
+const userPasswordValue = userPassword.value;
+
+// handle sign in functionality
+btnSignIn.addEventListener('click', () => {
+
+})
