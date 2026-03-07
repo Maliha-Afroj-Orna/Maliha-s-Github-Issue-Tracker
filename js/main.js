a0bbadbaa6119aeb3bed9cf921ba8fa5c14ccd3e
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-07 21:01:44 +0600

    tabs section added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('main connecetd')
+// tab section starts
+const allTab = document.getElementById("all-tab");
+const openTab = document.getElementById("open-tab");
+const closeTab = document.getElementById("close-tab");
+const activeBtn = ["btn", "btn-primary"];
+const inActiveBtn = ["btn", "btn-outline"];
+
+const toggleButton = (id) => {
+  allTab.classList.remove(...activeBtn);
+  openTab.classList.remove(...activeBtn);
+  closeTab.classList.remove(...activeBtn);
+
+  allTab.classList.add(...inActiveBtn);
+  openTab.classList.add(...inActiveBtn);
+  closeTab.classList.add(...inActiveBtn);
+
+  const clickedBtn = document.getElementById(id);
+  clickedBtn.classList.remove(...inActiveBtn);
+  clickedBtn.classList.add(...activeBtn);
+};
