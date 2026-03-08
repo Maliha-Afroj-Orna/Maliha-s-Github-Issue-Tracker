67f33f9365eca4b0da8e33ba73f895e803382412
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-08 22:33:44 +0600

    made the website responsive

--- js/login.js
+++ js/login.js
@@ -1,22 +1,20 @@
 // login page starts
-const btnSignIn = document.getElementById('btn-signIn');
-const userInput = document.getElementById('user-input');
-const userPassword = document.getElementById('user-password');
+const btnSignIn = document.getElementById("btn-signIn");
+const userInput = document.getElementById("user-input");
+const userPassword = document.getElementById("user-password");
 
 // handle sign in functionality
-btnSignIn.addEventListener('click', () => {
-    // user input
-    const userInputValue = userInput.value;
-    
-    // password
-    const userPasswordValue = userPassword.value;
-    
-    if(userInputValue === 'admin' && userPasswordValue === 'admin123') {
-        alert('Login Successful!');
-        window.location.assign('./main.html');
-    }
-    else {
-        alert("Invalid Username or Password!");
-    }
+btnSignIn.addEventListener("click", () => {
+  // user input
+  const userInputValue = userInput.value;
 
-})
+  // password
+  const userPasswordValue = userPassword.value;
+
+  if (userInputValue === "admin" && userPasswordValue === "admin123") {
+    alert("Login Successful!");
+    window.location.assign("./main.html");
+  } else {
+    alert("Invalid Username or Password!");
+  }
+});
