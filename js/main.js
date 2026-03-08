c7a9817d6cce67c529057a3cbe5d0acb4a39f3de
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-08 14:32:52 +0600

    added top border in open and close cards

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ const displayAllData = (issues) => {
     const card = document.createElement("div");
     card.innerHTML = `
         <div onclick="openModal(${issue.id})" class="card bg-white card-md shadow-xl border border-gray-100 h-[94%] cursor-pointer">
-            <div class="card-body space-y-2">
+            <div class="card-body space-y-2 ${issue.status == 'open' ? "border-t-4 border-[#00A96E] rounded-xl" : "border-t-4 border-[#A855F7] rounded-xl"}">
               <div class="flex justify-between items-center">
                 <img src="./assets/Open-Status.png" alt="Open Status" />
                 <div
@@ -110,32 +110,45 @@ const displayAllData = (issues) => {
 };
 loadAllCards();
 
-
 // modal section starts
-const issueModal = document.getElementById('issue_modal');
+const issueModal = document.getElementById("issue_modal");
 const openModal = async (id) => {
-  const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`,
+  );
   const data = await res.json();
   const issueDetails = data.data;
 
-  const modalTitle = document.getElementById('modalTitle');
-  const modalStatus = document.getElementById('modalStatus');
-  const modalName = document.getElementById('modalName');
-  const modalDate = document.getElementById('modalDate');
-  const modalBadge1 = document.getElementById('modalBadge1');
-  const modalBadge2 = document.getElementById('modalBadge2');
-  const modalDesc = document.getElementById('modalDesc');
-  const modalFooterName = document.getElementById('modalFooterName');
-  const modalFooterBadge = document.getElementById('modalFooterBadge');
+  const modalTitle = document.getElementById("modalTitle");
+  const modalStatus = document.getElementById("modalStatus");
+  const modalName = document.getElementById("modalName");
+  const modalDate = document.getElementById("modalDate");
+  const modalBadge = document.getElementById("modalBadge");
+  const modalDesc = document.getElementById("modalDesc");
+  const modalFooterName = document.getElementById("modalFooterName");
+  const modalFooterBadge = document.getElementById("modalFooterBadge");
 
   modalTitle.innerText = issueDetails.title;
-  modalStatus.innerHTML = issueDetails.status;
+  modalStatus.innerHTML = `${issueDetails.status == "open" ? `<button id="modalStatus" class="btn btn-success rounded-3xl text-white font-medium text-sm px-6"> ${issueDetails.status}</button>` : `<button id="modalStatus" class="btn btn-error rounded-3xl text-white font-medium text-sm px-6"> ${issueDetails.status}</button>`}`;
   modalName.innerText = issueDetails.author;
   modalDate.innerText = issueDetails.createdAt;
+  modalBadge.innerHTML = `${issueDetails.labels
+    .map((label) => {
+      return `<div
+                        class="badge badge-error text-[9px] ${label.toUpperCase() == "BUG" ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444]" : label.toUpperCase() == "HELP WANTED" ? "text-[#D97706] bg-[#FFF8DB] border-[#D97706]" : label.toUpperCase() == "ENHANCEMENT" ? "text-[#00A96E] bg-[#BBF7D0] border-[#00A96E]" : label.toUpperCase() == "GOOD FIRST ISSUE" ? "text-purple-700 bg-purple-400 border-purple-800" : "text-sky-700 bg-sky-400 border-sky-800"} font-medium rounded-3xl"
+                        >
+                        ${label.toUpperCase() == "BUG" ? '<i class="fa-solid fa-bug text-[#EF4444]"></i>' : label.toUpperCase() == "HELP WANTED" ? '<i class="fa-solid fa-compact-disc"></i>' : label.toUpperCase() == "ENHANCEMENT" ? '<i class="fa-solid fa-star"></i>' : label.toUpperCase() == "good first issue" ? '<i class="fa-solid fa-circle-exclamation"></i>' : '<i class="fa-solid fa-file-circle-exclamation"></i>'} ${label.toUpperCase()}
+                        </div>
+                        `;
+    })
+    .join(" ")}`;
   modalDesc.innerText = issueDetails.description;
   modalTitle.innerText = issueDetails.title;
   modalFooterName.innerText = issueDetails.assignee.toUpperCase();
-  modalFooterBadge.innerText = issueDetails.priority.toUpperCase();
+  modalFooterBadge.innerHTML = `${issueDetails.priority.toUpperCase() == "HIGH" ? `<div class="btn btn-error text-xs text-white bg-[#EF4444] border-[#EF4444] font-medium rounded-3xl px-6"> ${issueDetails.priority.toUpperCase()} </div>` : issueDetails.priority.toUpperCase() == "MEDIUM" ? `<div class="btn btn-error text-xs text-white bg-[#D97706] border-[#D97706] font-medium rounded-3xl px-6">${issueDetails.priority.toUpperCase()}</div>` : `<div class="btn btn-error text-xs text-white bg-[#9CA3AF] border-[#9CA3AF] font-medium rounded-3xl px-6">${issueDetails.priority.toUpperCase()}</div>`}`;
 
   issueModal.showModal();
-}
+};
+
+
+// search section starts
