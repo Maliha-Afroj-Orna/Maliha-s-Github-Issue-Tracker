d47715675dbd3c5179af84585fcb4b946041f372
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-08 13:39:36 +0600

    create the modal and shows all the details for specific card

--- js/main.js
+++ js/main.js
@@ -42,13 +42,13 @@ const loadAllCards = async () => {
   totalNumOfCards.innerText = issues.length;
 };
 // load open cards
-const loadOpenCards = async () => {
+const loadOpenCards = () => {
   const openIssues = issues.filter((issue) => issue.status == "open");
   displayAllData(openIssues);
   totalNumOfCards.innerText = openIssues.length;
 };
 // load close cards
-const loadCloseCards = async () => {
+const loadCloseCards = () => {
   const closeIssues = issues.filter((issue) => issue.status == "closed");
   displayAllData(closeIssues);
   totalNumOfCards.innerText = closeIssues.length;
@@ -60,7 +60,7 @@ const displayAllData = (issues) => {
   issues.forEach((issue) => {
     const card = document.createElement("div");
     card.innerHTML = `
-        <div class="card bg-white card-md shadow-xl border border-gray-100 h-[94%]">
+        <div onclick="openModal(${issue.id})" class="card bg-white card-md shadow-xl border border-gray-100 h-[94%] cursor-pointer">
             <div class="card-body space-y-2">
               <div class="flex justify-between items-center">
                 <img src="./assets/Open-Status.png" alt="Open Status" />
@@ -80,7 +80,7 @@ const displayAllData = (issues) => {
                 ${issue.labels
                   .map((label) => {
                     return `<div
-                        class="badge badge-error text-[9px] ${label.toUpperCase() == "BUG" ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444]" : label.toUpperCase() == "HELP WANTED" ? "text-[#D97706] bg-[#FFF8DB] border-[#D97706]" : label.toUpperCase() == "ENHANCEMENT" ? "text-[#00A96E] bg-[#BBF7D0] border-[#00A96E]" : label.toUpperCase() == "GOOD FIRST ISSUE" ? "text-purple-700 bg-purple-400 border-purple-800" : "text-sky-700 bg-sky-400 border-sky-800"} font-medium"
+                        class="badge badge-error text-[9px] ${label.toUpperCase() == "BUG" ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444]" : label.toUpperCase() == "HELP WANTED" ? "text-[#D97706] bg-[#FFF8DB] border-[#D97706]" : label.toUpperCase() == "ENHANCEMENT" ? "text-[#00A96E] bg-[#BBF7D0] border-[#00A96E]" : label.toUpperCase() == "GOOD FIRST ISSUE" ? "text-purple-700 bg-purple-400 border-purple-800" : "text-sky-700 bg-sky-400 border-sky-800"} font-medium rounded-3xl"
                         >
                         ${label.toUpperCase() == "BUG" ? '<i class="fa-solid fa-bug text-[#EF4444]"></i>' : label.toUpperCase() == "HELP WANTED" ? '<i class="fa-solid fa-compact-disc"></i>' : label.toUpperCase() == "ENHANCEMENT" ? '<i class="fa-solid fa-star"></i>' : label.toUpperCase() == "good first issue" ? '<i class="fa-solid fa-circle-exclamation"></i>' : '<i class="fa-solid fa-file-circle-exclamation"></i>'} ${label.toUpperCase()}
                         </div>
@@ -109,3 +109,33 @@ const displayAllData = (issues) => {
   });
 };
 loadAllCards();
+
+
+// modal section starts
+const issueModal = document.getElementById('issue_modal');
+const openModal = async (id) => {
+  const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+  const data = await res.json();
+  const issueDetails = data.data;
+
+  const modalTitle = document.getElementById('modalTitle');
+  const modalStatus = document.getElementById('modalStatus');
+  const modalName = document.getElementById('modalName');
+  const modalDate = document.getElementById('modalDate');
+  const modalBadge1 = document.getElementById('modalBadge1');
+  const modalBadge2 = document.getElementById('modalBadge2');
+  const modalDesc = document.getElementById('modalDesc');
+  const modalFooterName = document.getElementById('modalFooterName');
+  const modalFooterBadge = document.getElementById('modalFooterBadge');
+
+  modalTitle.innerText = issueDetails.title;
+  modalStatus.innerHTML = issueDetails.status;
+  modalName.innerText = issueDetails.author;
+  modalDate.innerText = issueDetails.createdAt;
+  modalDesc.innerText = issueDetails.description;
+  modalTitle.innerText = issueDetails.title;
+  modalFooterName.innerText = issueDetails.assignee.toUpperCase();
+  modalFooterBadge.innerText = issueDetails.priority.toUpperCase();
+
+  issueModal.showModal();
+}
