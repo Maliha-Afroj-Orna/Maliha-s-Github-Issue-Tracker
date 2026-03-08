bb610cfc90320ba9aa80d19e75beb7d7f916956f
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-08 12:18:39 +0600

    shows all cards, oepn cards and close cards perfectly

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const openTab = document.getElementById("open-tab");
 const closeTab = document.getElementById("close-tab");
 const activeBtn = ["btn", "btn-primary"];
 const inActiveBtn = ["btn", "btn-outline"];
-const allCards = document.getElementById('allCards');
-const totalNumOfCards = document.getElementById('totalNumOfCards');
+const totalNumOfCards = document.getElementById("totalNumOfCards");
+const cardContainer = document.getElementById("cardContainer");
+let issues = [];
 
 const toggleButton = (id) => {
   allTab.classList.remove(...activeBtn);
@@ -21,30 +22,50 @@ const toggleButton = (id) => {
   clickedBtn.classList.add(...activeBtn);
 
   // card section starts
-  if(clickedBtn == 'allTab') {
+  if (id == "all-tab") {
     loadAllCards();
-    totalNumOfCards.innerText = issues.length;
+  } else if (id == "open-tab") {
+    loadOpenCards();
+  } else if (id == "close-tab") {
+    loadCloseCards();
   }
 };
 
 // load all cards
 const loadAllCards = async () => {
-    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-    const data = await res.json();
-    displayAllData(data.data);
-}
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await res.json();
+  issues = data.data;
+  displayAllData(issues);
+  totalNumOfCards.innerText = issues.length;
+};
+// load open cards
+const loadOpenCards = async () => {
+  const openIssues = issues.filter((issue) => issue.status == "open");
+  displayAllData(openIssues);
+  totalNumOfCards.innerText = openIssues.length;
+};
+// load close cards
+const loadCloseCards = async () => {
+  const closeIssues = issues.filter((issue) => issue.status == "closed");
+  displayAllData(closeIssues);
+  totalNumOfCards.innerText = closeIssues.length;
+};
+// display cards
 const displayAllData = (issues) => {
-    allCards.innerHTML = "";
+  cardContainer.innerHTML = "";
 
-    issues.forEach(issue => {
-        const card = document.createElement('div');
-        card.innerHTML = `
+  issues.forEach((issue) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
         <div class="card bg-white card-md shadow-xl border border-gray-100 h-[94%]">
             <div class="card-body space-y-2">
               <div class="flex justify-between items-center">
                 <img src="./assets/Open-Status.png" alt="Open Status" />
                 <div
-                  class="badge badge-error text-xs (${issue.priority.toUpperCase() == 'HIGH' ? 'text-[#EF4444] bg-[#FEECEC] border-[#FEECEC]' : issue.priority.toUpperCase() == 'MEDIUM' ? 'text-[#F59E0B] bg-[#FFF6D1] border-[#FFF6D1]' : 'text-[#9CA3AF] bg-[#EEEFF2] border-[#EEEFF2]'}  font-medium"
+                  class="badge badge-error text-xs ${issue.priority.toUpperCase() == "HIGH" ? "text-[#EF4444] bg-[#FEECEC] border-[#FEECEC]" : issue.priority.toUpperCase() == "MEDIUM" ? "text-[#F59E0B] bg-[#FFF6D1] border-[#FFF6D1]" : "text-[#9CA3AF] bg-[#EEEFF2] border-[#EEEFF2]"}  font-medium"
                 >
                   ${issue.priority.toUpperCase()}
                 </div>
@@ -56,34 +77,35 @@ const displayAllData = (issues) => {
                 ${issue.description}
               </p>
               <div class="flex gap-2">
-                ${issue.labels.map(label => {
+                ${issue.labels
+                  .map((label) => {
                     return `<div
-                        class="badge badge-error text-[9px] ${label.toUpperCase() == 'BUG' ? 'text-[#EF4444] bg-[#FEECEC] border-[#EF4444]' : label.toUpperCase() == 'HELP WANTED' ? 'text-[#D97706] bg-[#FFF8DB] border-[#D97706]' : label.toUpperCase() == 'ENHANCEMENT' ? 'text-[#00A96E] bg-[#BBF7D0] border-[#00A96E]' : label.toUpperCase() == 'GOOD FIRST ISSUE' ? 'text-purple-700 bg-purple-400 border-purple-800' : 'text-sky-700 bg-sky-400 border-sky-800'} font-medium"
+                        class="badge badge-error text-[9px] ${label.toUpperCase() == "BUG" ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444]" : label.toUpperCase() == "HELP WANTED" ? "text-[#D97706] bg-[#FFF8DB] border-[#D97706]" : label.toUpperCase() == "ENHANCEMENT" ? "text-[#00A96E] bg-[#BBF7D0] border-[#00A96E]" : label.toUpperCase() == "GOOD FIRST ISSUE" ? "text-purple-700 bg-purple-400 border-purple-800" : "text-sky-700 bg-sky-400 border-sky-800"} font-medium"
                         >
-                        ${label.toUpperCase() == 'BUG' ? '<i class="fa-solid fa-bug text-[#EF4444]"></i>' : label.toUpperCase() == 'HELP WANTED' ? '<i class="fa-solid fa-compact-disc"></i>' : label.toUpperCase() == 'ENHANCEMENT' ? '<i class="fa-solid fa-star"></i>' : label.toUpperCase() == 'good first issue' ? '<i class="fa-solid fa-circle-exclamation"></i>' : '<i class="fa-solid fa-file-circle-exclamation"></i>'} ${label.toUpperCase()}
+                        ${label.toUpperCase() == "BUG" ? '<i class="fa-solid fa-bug text-[#EF4444]"></i>' : label.toUpperCase() == "HELP WANTED" ? '<i class="fa-solid fa-compact-disc"></i>' : label.toUpperCase() == "ENHANCEMENT" ? '<i class="fa-solid fa-star"></i>' : label.toUpperCase() == "good first issue" ? '<i class="fa-solid fa-circle-exclamation"></i>' : '<i class="fa-solid fa-file-circle-exclamation"></i>'} ${label.toUpperCase()}
                         </div>
-                        `
-                }).join(' ')}
+                        `;
+                  })
+                  .join(" ")}
                 
               </div>
 
               <hr class="text-gray-200 w-full" />
 
-              <div class="space-y-1">
-                <p class="text-[#64748B] text-sm">#1 by ${issue.author}</p>
-                <p class="text-[#64748B] text-sm pb-2">${issue.createdAt}</p>
+              <div class="space-y-1 flex justify-between items-center pb-2">
+                <div>
+                  <p class="text-[#64748B] text-xs">#${issue.id} by ${issue.author}</p>
+                  <p class="text-[#64748B] text-xs">Assignee: ${issue.assignee}</p>
+                </div>
+                <div>
+                  <p class="text-[#64748B] text-xs ">${new Date(issue.createdAt).toLocaleDateString("en-US")}</p>
+                  <p class="text-[#64748B] text-xs ">Updated: ${new Date(issue.updatedAt).toLocaleDateString("en-US")}</p>
+                </div>
               </div>
             </div>
           </div>
         `;
-        allCards.append(card);
-        totalNumOfCards.innerText = issues.length;
-    })
-}
+    cardContainer.append(card);
+  });
+};
 loadAllCards();
-
-
-
-
-
-
